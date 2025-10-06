c520f4490131db65c845e85b37c6bc2fd6e6b53f
Agaba Andre <agabaandre@gmail.com>
2025-10-06 13:58:59 +0300

    Fix package description display in selected package card
    - Add description div to updateSelectedPackageInfo() function
    - Ensure description area is present in both normal flow and error flow
    - Description will now show in the selected package card when package is selected

--- js/registration.js
+++ js/registration.js
@@ -573,6 +573,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     <h4>${selectedPackage.name}</h4>
                     <p>${selectedPackage.type.charAt(0).toUpperCase() + selectedPackage.type.slice(1)} Package</p>
                     ${priceDisplay}
+                    <div class="package-description mt-3" id="packageDescription" style="display: none;">
+                        <small class="text-muted" id="packageDescriptionText">Loading description...</small>
+                    </div>
                 </div>
             `;
         }
